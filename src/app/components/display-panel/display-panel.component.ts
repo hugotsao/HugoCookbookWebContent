0f29fe17b7128463d2d57552da6cb5a40f69d4fb
Hugo <>
2020-09-30 15:15:01 -0400

    create new blog link and new article object

--- src/app/components/display-panel/display-panel.component.ts
+++ src/app/components/display-panel/display-panel.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import * as marked from "marked";
 import { DisplayService } from '../../services/display.service';
 import { DataStoreService } from '../../services/data-store.service';
 import { ActivatedRoute } from '@angular/router';
@@ -32,20 +31,37 @@ export class DisplayPanelComponent implements OnInit {
       this.articleId = +paramMap.get('articleId');
       
       this.getArticle();
+
     })
   }
+  
+
   getArticle() {
     this.displayService.getArticleFromId(this.articleId).subscribe(
       article => {
-        this.article = article;
+        this.article = article;        
+        if (this.articleId === -1 && this.editorView) {
+          this.article = {
+            ...this.article,
+            articleId: this.articleId + 1,
+            title: '',
+            categoryId: 1,
+            tags: [],
+            ref: [],
+            DateCreation: new Date(),
+            DateLastModified: new Date()
+          };
+          this.content = '_new content here_';
+        } else {
         this.articleId = article.articleId;
-        this.renderContent();        
+        this.renderContent();
+        }
       })
   }
   renderContent() {
     this.dataStoreService.fetchContent(this.articleId).subscribe(
-      res => this.content = marked(res.content)
-    )    
+      res => this.content = res.content
+    )  
   }
 
 }
